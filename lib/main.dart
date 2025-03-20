63ccbfe65f64cf7d9594f4d0cda3169cd71651d0
Muhammad <mukhammaddev9@gmail.com>
2025-03-20 19:00:02 +0500

    set

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 void main() {
-  List<int> nums = [4, 5];
-  nums.add(1);
-  nums.addAll([4, 6, 7, 0]);
+  // List<int> nums = [4, 5];
+  // nums.add(1);
+  // nums.addAll([4, 6, 7, 0]);
+  //
+  // nums.remove(4);
+  // nums.removeAt(5);
+  //
+  // print('First: ${nums.first}. Last: ${nums.last}. Length: ${nums.length}');
 
-  nums.remove(4);
-  nums.removeAt(5);
+  Set<int> digits = {1, 2, 3, 4, 5, 5};
 
-  print('First: ${nums.first}. Last: ${nums.last}. Length: ${nums.length}');
+  print(digits);
 }
