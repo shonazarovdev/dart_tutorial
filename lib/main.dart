86587b4f500749802cac82dec5a95d8f5f55372e
Muhammad <mukhammaddev9@gmail.com>
2025-03-20 18:55:01 +0500

    list

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
 void main() {
-  print('hello');
+  List<int> nums = [4, 5];
+  nums.add(1);
+  nums.addAll([4, 6, 7, 0]);
+
+  nums.remove(4);
+  nums.removeAt(5);
+
+  print('First: ${nums.first}. Last: ${nums.last}. Length: ${nums.length}');
 }
